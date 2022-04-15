9fa55397d067594859bccd8cc89d8a6743183864
Ali Dehghani <alixdehghani@gmail.com>
2022-04-15 18:07:32 +0430

    stable version 1.0

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,18 +1,12 @@
-import { NgModule } from "@angular/core";
-import { NavbarComponent } from "./header/navbar.component";
-import {MatButtonModule} from '@angular/material/button';
-import { FooterComponent } from "./footer/footer.component";
-
+import { NgModule } from '@angular/core';
+import { NavbarComponent } from './header/navbar.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-    declarations: [
-        NavbarComponent,
-        FooterComponent
-    ],
-    imports: [MatButtonModule],
-    exports: [
-        NavbarComponent,
-        FooterComponent
-    ]
+  declarations: [NavbarComponent, FooterComponent],
+  imports: [MatButtonModule, MatIconModule],
+  exports: [NavbarComponent, FooterComponent],
 })
-export class CoreModule { }
+export class CoreModule {}
