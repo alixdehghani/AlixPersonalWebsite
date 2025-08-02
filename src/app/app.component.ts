fc6fc156c82d1072baef661c3b8839dd461485fc
ali dehghani <ali.dehghani@faraz.com>
2025-08-02 14:24:49 +0330

    update for telegram web app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+declare var Telegram: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AlixPersonalWebsite';
+
+  ngOnInit() {
+    // Initialize Telegram Web App
+    const tg = Telegram.WebApp;
+
+    tg.expand();
+    const user = tg.initDataUnsafe?.user;
+    if (user) {
+      console.log("Hello, " + user.first_name);
+    }
+
+    tg.MainButton.setText("Submit");
+    tg.MainButton.show();
+  }
 }
